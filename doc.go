00294816ffe951796f2a96a577cdcdf760166ec7
Aaron Jacobs <jacobsa@google.com>
2015-02-27 09:26:52 +1100

    Added a basic package description.

---
+++ doc.go
@@ -0,0 +1,5 @@
+// Copyright 2015 Google Inc. All Rights Reserved.
+// Author: jacobsa@google.com (Aaron Jacobs)
+
+// Package fuse enables writing and mounting user-space file systems.
+package fuse
