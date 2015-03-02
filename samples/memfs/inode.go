2f98fdd29d7a8161df39e987093abcc0b01a39ce
Aaron Jacobs <jacobsa@google.com>
2015-03-02 14:17:52 +1100

    Added a TODO.

--- samples/memfs/inode.go
+++ samples/memfs/inode.go
@@ -4,6 +4,10 @@
 package memfs
 
 // Common attributes for files and directories.
+//
+// TODO(jacobsa): Add tests for interacting with a file/directory after it has
+// been unlinked, including creating a new file. Make sure we don't screw up
+// and reuse the inode while it is still in use.
 type inode struct {
 	// The *memFile or *memDir for this inode, or nil if the inode is available
 	// for reuse.
