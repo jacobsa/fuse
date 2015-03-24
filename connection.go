be2f3fc73eee755069781861f203afa3e382e9db
Aaron Jacobs <jacobsa@google.com>
2015-03-24 15:23:19 +1100

    Added a warning on Connection.ReadOp.

--- connection.go
+++ connection.go
@@ -14,12 +14,28 @@
 
 package fuse
 
-import "github.com/jacobsa/fuse/fuseops"
+import (
+	"log"
+
+	"github.com/jacobsa/bazilfuse"
+	"github.com/jacobsa/fuse/fuseops"
+)
 
 // A connection to the fuse kernel process.
 type Connection struct {
+	logger *log.Logger
+	wrapped
 }
+
+func newConnection(wrapped *bazilfuse.Conn) (c *Connection, err error)
 
 // Read the next op from the kernel process. Return io.EOF if the kernel has
 // closed the connection.
+//
+// This function delivers ops in exactly the order they are received from
+// /dev/fuse. Be wary of naively calling it concurrently: you probably want
+// ordering guarantees between e.g. write ops and flush ops. For example,
+// close(2) causes WriteFileOps to be issued before a FlushFileOp, but doesn't
+// wait for their response before issuing the latter (cf.
+// https://github.com/jacobsa/fuse/issues/3).
 func (c *Connection) ReadOp() (op fuseops.Op, err error)
