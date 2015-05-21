bc969387cae8918cb8d84ef95e059cfcbc19f474
Aaron Jacobs <jacobsa@google.com>
2015-05-21 15:22:26 +1000

    Fixed memfs bugs.

--- samples/memfs/memfs.go
+++ samples/memfs/memfs.go
@@ -301,6 +301,14 @@ func (fs *memFS) MkDir(
 	parent := fs.getInodeForModifyingOrDie(op.Parent)
 	defer parent.mu.Unlock()
 
+	// Ensure that the name doesn't already exist, so we don't wind up with a
+	// duplicate.
+	_, exists := parent.LookUpChild(op.Name)
+	if exists {
+		err = fuse.EEXIST
+		return
+	}
+
 	// Set up attributes from the child, using the credentials of the calling
 	// process as owner (matching inode_init_owner, cf. http://goo.gl/5qavg8).
 	childAttrs := fuseops.InodeAttributes{
@@ -341,7 +349,7 @@ func (fs *memFS) CreateFile(
 	parent := fs.getInodeForModifyingOrDie(op.Parent)
 	defer parent.mu.Unlock()
 
-	// Ensure that the name doesn't alread exist, so we don't wind up with a
+	// Ensure that the name doesn't already exist, so we don't wind up with a
 	// duplicate.
 	_, exists := parent.LookUpChild(op.Name)
 	if exists {
@@ -395,6 +403,14 @@ func (fs *memFS) CreateSymlink(
 	// Grab the parent, which we will update shortly.
 	parent := fs.getInodeForModifyingOrDie(op.Parent)
 	defer parent.mu.Unlock()
+
+	// Ensure that the name doesn't already exist, so we don't wind up with a
+	// duplicate.
+	_, exists := parent.LookUpChild(op.Name)
+	if exists {
+		err = fuse.EEXIST
+		return
+	}
 
 	// Set up attributes from the child, using the credentials of the calling
 	// process as owner (matching inode_init_owner, cf. http://goo.gl/5qavg8).
