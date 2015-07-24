ed2ae82923cfdcc8e5039f259755bf61f0826710
Aaron Jacobs <jacobsa@google.com>
2015-07-24 16:35:54 +1000

    Deleted fuseshim.

--- internal/fuseshim/options_freebsd.go
+++
@@ -1,9 +0,0 @@
-package fuseshim
-
-func localVolume(conf *mountConfig) error {
-	return nil
-}
-
-func volumeName(name string) MountOption {
-	return dummyOption
-}
--- internal/fuseshim/unmount.go
+++
@@ -1,6 +0,0 @@
-package fuseshim
-
-// Unmount tries to unmount the filesystem mounted at dir.
-func Unmount(dir string) error {
-	return unmount(dir)
-}
