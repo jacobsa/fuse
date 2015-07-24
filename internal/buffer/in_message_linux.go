d93a81b046e4eb1b48559bec350b25d763847ccd
Aaron Jacobs <jacobsa@google.com>
2015-07-24 14:46:45 +1000

    Defined the constant for Linux, too.

--- internal/buffer/in_message_linux.go
+++ internal/buffer/in_message_linux.go
@@ -16,6 +16,6 @@ package buffer
 
 // The maximum fuse write request size that InMessage can acommodate.
 //
-// Experimentally, OS X appears to cap the size of writes to 1 MiB, regardless
-// of whether a larger size is specified in the mount options.
-const MaxWriteSize = 1 << 20
+// Experimentally, Linux appears to refuse to honor a MaxWrite setting in an
+// INIT response of more than 128 KiB.
+const MaxWriteSize = 1 << 17
