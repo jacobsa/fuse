8b7833ba01887382f298a1c9b287f2e96924eb4a
Aaron Jacobs <jacobsa@google.com>
2016-02-29 13:40:11 +1100

    TestNonEmptyMountPoint: clean up in the event of a successful mount.

--- mount_test.go
+++ mount_test.go
@@ -66,6 +66,8 @@ func TestSuccessfulMount(t *testing.T) {
 }
 
 func TestNonEmptyMountPoint(t *testing.T) {
+	ctx := context.Background()
+
 	// Set up a temporary directory.
 	dir, err := ioutil.TempDir("", "mount_test")
 	if err != nil {
@@ -82,13 +84,19 @@ func TestNonEmptyMountPoint(t *testing.T) {
 
 	// Attempt to mount.
 	fs := &minimalFS{}
-	_, err = fuse.Mount(
+	mfs, err := fuse.Mount(
 		dir,
 		fuseutil.NewFileSystemServer(fs),
 		&fuse.MountConfig{})
 
+	if err == nil {
+		fuse.Unmount(mfs.Dir())
+		mfs.Join(ctx)
+		t.Fatal("fuse.Mount returned nil")
+	}
+
 	const want = "not empty"
-	if err == nil || !strings.Contains(err.Error(), want) {
-		t.Errorf("Unexpected error: %v", err)
+	if got := err.Error(); !strings.Contains(got, want) {
+		t.Errorf("Unexpected error: %v", got)
 	}
 }
