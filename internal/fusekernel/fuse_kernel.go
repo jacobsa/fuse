caf69d3f2cd11ea960f5a7e4ab200d5deb03dd6b
Kurt Jensen <kjensen@dnanexus.com>
2021-05-21 17:16:12 +0000

    Lower min minor version to 19 for osxfuse

--- internal/fusekernel/fuse_kernel.go
+++ internal/fusekernel/fuse_kernel.go
@@ -44,7 +44,7 @@ import (
 // The FUSE version implemented by the package.
 const (
 	ProtoVersionMinMajor = 7
-	ProtoVersionMinMinor = 28
+	ProtoVersionMinMinor = 19
 	ProtoVersionMaxMajor = 7
 	ProtoVersionMaxMinor = 31
 )
