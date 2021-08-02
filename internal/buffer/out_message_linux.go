a6d2b09eba75fd1494fe0186d58b3556f6fee0e1
Kurt Jensen <kjensen@dnanexus.com>
2021-08-02 20:27:38 +0000

    Update link to fuse_i.h source

--- internal/buffer/out_message_linux.go
+++ internal/buffer/out_message_linux.go
@@ -17,5 +17,5 @@ package buffer
 // The maximum read size that we expect to ever see from the kernel, used for
 // calculating the size of out messages.
 //
-// For 4 KiB pages, this is 1024 KiB (cf. https://goo.gl/HOiEYo)
+// For 4 KiB pages, this is 1024 KiB (cf. https://github.com/torvalds/linux/blob/15db16837a35d8007cb8563358787412213db25e/fs/fuse/fuse_i.h#L38-L40)
 const MaxReadSize = 1 << 20
