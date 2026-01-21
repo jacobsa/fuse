a945b3d245402bb5ea9b735615ed2c3c857e876c
Scott Bauersfeld <scott.bauersfeld@databricks.com>
2026-01-21 21:08:30 +0000

    better test

--- samples/killprivfs/killpriv_fs_test.go
+++ samples/killprivfs/killpriv_fs_test.go
@@ -69,58 +69,41 @@ func (t *KillPrivFSTest) TestMountWithKillPrivV2() {
 
 func (t *KillPrivFSTest) TestWriteToSetuidFile() {
 	if syscall.Getuid() != 0 {
-		// Skip test if not root
 		return
 	}
 
 	filePath := path.Join(t.Dir, "setuid_test.txt")
 
-	// Create a file as root
 	err := ioutil.WriteFile(filePath, []byte("initial"), 0644)
 	AssertEq(nil, err)
 
-	// Set the setuid bit
 	err = os.Chmod(filePath, 04755)
 	AssertEq(nil, err)
 
-	// Verify setuid bit is set
 	stat, err := os.Stat(filePath)
 	AssertEq(nil, err)
 	ExpectTrue((stat.Mode() & os.ModeSetuid) != 0, "setuid bit should be set")
 
 	t.fs.ResetFlags()
 
-	// Write to the file as a non-root user (nobody - uid 65534)
-	// We use a helper script approach to drop privileges
 	cmd := exec.Command("su", "-s", "/bin/sh", "nobody", "-c",
 		"echo 'test data' >> "+filePath)
-	_, err = cmd.CombinedOutput()
-
-	// Check if su succeeded
+	output, err := cmd.CombinedOutput()
 	if err != nil {
-		// su command failed - this is expected if 'nobody' user doesn't exist
-		// Skip flag verification - unable to drop privileges
-		return
+		AssertEq(nil, err, "su command failed: %s", string(output))
 	}
 
-	// If we successfully wrote as nobody, check if flag was set
 	_, _, writeFlag, _ := t.fs.GetFlags()
-
-	// The kernel should have set WriteKillSuidgid flag when a non-root user
-	// without CAP_FSETID writes to a setuid file
-	// Note: The flag may not be set depending on kernel version and configuration
-	_ = writeFlag
+	ExpectTrue(writeFlag, "WriteKillSuidgid flag should be set when non-root writes to setuid file")
 }
 
 func (t *KillPrivFSTest) TestSetuidBitWithChown() {
 	if syscall.Getuid() != 0 {
-		// Skip test if not root
 		return
 	}
 
 	filePath := path.Join(t.Dir, "chown_test.txt")
 
-	// Create a file with setuid bit
 	err := ioutil.WriteFile(filePath, []byte("test"), 0644)
 	AssertEq(nil, err)
 
@@ -129,26 +112,20 @@ func (t *KillPrivFSTest) TestSetuidBitWithChown() {
 
 	t.fs.ResetFlags()
 
-	// Change ownership (this should trigger SetInodeAttributes with KillSuidgid)
 	err = os.Chown(filePath, 1000, 1000)
-	if err != nil {
-		// Chown failed (may be expected in test environment)
-		return
-	}
+	AssertEq(nil, err)
 
 	_, _, _, setattrFlag := t.fs.GetFlags()
-	_ = setattrFlag
+	ExpectTrue(setattrFlag, "SetattrKillSuidgid flag should be set when changing ownership of setuid file")
 }
 
 func (t *KillPrivFSTest) TestTruncateSetuidFile() {
 	if syscall.Getuid() != 0 {
-		// Skip test if not root
 		return
 	}
 
 	filePath := path.Join(t.Dir, "truncate_test.txt")
 
-	// Create a file with setuid bit
 	err := ioutil.WriteFile(filePath, []byte("test data here"), 0644)
 	AssertEq(nil, err)
 
@@ -157,18 +134,15 @@ func (t *KillPrivFSTest) TestTruncateSetuidFile() {
 
 	t.fs.ResetFlags()
 
-	// Truncate as nobody user
 	cmd := exec.Command("su", "-s", "/bin/sh", "nobody", "-c",
 		"truncate -s 5 "+filePath)
-	_, err = cmd.CombinedOutput()
-
+	output, err := cmd.CombinedOutput()
 	if err != nil {
-		// truncate command failed
-		return
+		AssertEq(nil, err, "truncate command failed: %s", string(output))
 	}
 
 	_, _, _, setattrFlag := t.fs.GetFlags()
-	_ = setattrFlag
+	ExpectTrue(setattrFlag, "SetattrKillSuidgid flag should be set when non-root truncates setuid file")
 }
 
 func (t *KillPrivFSTest) TestBasicOperationsStillWork() {
